3d961f602b65fc93aea2ecdd41768cd64a3ad804
nehaneosoft <neha.kumari@neosoftmail.com>
2022-03-03 12:23:52 +0530

    Initialized backend and frontend

---
+++ Backend/data/notes.js
@@ -0,0 +1,22 @@
+const notes = [
+    {
+        _id: "1",
+        title:"Day 1 of College",
+        content : "Introduced myself to all..",
+        category:"college",
+    },
+    {
+        _id: "2",
+        title:"Day 2 of College",
+        content : "Made friends",
+        category:"class",
+    },
+    {
+        _id: "3",
+        title:"Day 3 of College",
+        content : "Learned a lot",
+        category:"learning",
+    },
+];
+
+module.exports=notes;
